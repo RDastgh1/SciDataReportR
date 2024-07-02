cae4e472243c7ad89022b8eefafb4d6e7ec52de7
RDastgh1 <rdastgh1@jhmi.edu>
2024-07-02 19:32:40 -0400

    summarytable

--- R/CreateSummaryTable.R
+++ R/CreateSummaryTable.R
@@ -15,59 +15,50 @@
 #' @importFrom summarytools descr
 #' @export
 CreateSummaryTable <- function(Data, Variables = NULL, numdecimals = 2, Relabel = TRUE, Ordinal = FALSE, ScrollBoxHeight = "700px") {
-  if(is.null(Variables)){
-    Variables = colnames(Data)
+
+    if (is.null(Variables)) {
+      Variables = colnames(Data)
+    }
+    suppressWarnings({
+      Data <- Data %>% select(all_of(Variables))
+      l <- get_label(Data)
+      if(Ordinal){
+        Data <- ConvertOrdinalToNumeric(Data)
+        Data <- lapply(Data, as.numeric) %>% as.data.frame()
+        if(Relabel){
+          sjlabelled::set_label(Data) <- l
+        }
+      }
+
+      d <- summarytools::descr(Data)
+      statVars <- c("Mean", "Std.Dev", "Median", "IQR", "Min",
+                    "Max", "Skewness", "Kurtosis", "N.Valid", "Pct.Valid")
+      d2 <- as.data.frame(t(as.data.frame(d)))
+      d2 <- d2 %>% select(all_of(statVars))
+      d2 <- d2[Variables, ]
+      d2[statVars] <- lapply(d2[statVars], round, numdecimals)
+      if (Relabel) {
+        Data <- ReplaceMissingLabels(Data)
+        labels <- sjlabelled::get_label(Data, def.value = colnames(Data)) %>%
+          as.data.frame() %>% tibble::rownames_to_column()
+        colnames(labels) <- c("Variable", "label")
+        d2$label <- labels$label
+        d2 <- d2 %>% select(label, all_of(statVars))
+      }
+      SummaryTable <- d2 %>% rownames_to_column("Variable") %>%
+        mutate(Skewness = cell_spec(round(Skewness, 2), "html",
+                                    background = if_else(abs(Skewness) > 10, "yellow",
+                                                         "", missing = "grey")), Kurtosis = cell_spec(round(Kurtosis,
+                                                                                                            2), "html", background = if_else(abs(Kurtosis) >
+                                                                                                                                               10, "yellow", "", missing = "grey")), IQR = cell_spec(round(IQR,
+                                                                                                                                                                                                           2), "html", background = if_else(abs((IQR/Std.Dev/1.34) -
+                                                                                                                                                                                                                                                  1) > 0.5, "yellow", "", missing = "grey")), Pct.Valid = cell_spec(round(Pct.Valid,
+                                                                                                                                                                                                                                                                                                                          2), "html", background = if_else(Pct.Valid <
+                                                                                                                                                                                                                                                                                                                                                             70, "red", "", missing = "grey"))) %>% kable(format = "html",
+                                                                                                                                                                                                                                                                                                                                                                                                          escape = FALSE, digits = 2, row.names = TRUE, caption = "Descriptive Summary Table. IQR, Skewness, and Kurtosis are highlighted in yellow if they are indicative of a non-normal distribution. Pct.Valid is highlighted in red if over 30% of data is missing") %>%
+        kable_styling(bootstrap_options = c("striped", "hover",
+                                            "condensed", "responsive")) %>% scroll_box(width = "100%",
+                                                                                       height = ScrollBoxHeight)
+    })
+    return(SummaryTable)
   }
-
-
-
-  # Wrap the entire function body in suppressWarnings()
-  suppressWarnings({
-
-Data <- Data %>% select(all_of(Variables))
-# Extract labels before conversion
-l <- sjlabelled::get_label(Data)
-
-Data <- ConvertOrdinalToNumeric(Data)
-# Now Convert everything to numeric
-Data <- lapply(Data, as.numeric) %>% as.data.frame()
-
-# Read labels
-sjlabelled::set_label(Data)<- l
-
-
-  d <- summarytools::descr(Data)
-  statVars <- c('Mean', 'Std.Dev', 'Median', 'IQR', 'Min', 'Max', 'Skewness', 'Kurtosis', 'N.Valid', 'Pct.Valid')
-  d2 <- as.data.frame(t(as.data.frame(d)))
-  d2 <- d2 %>% select(all_of(statVars))
-  d2 <- d2[Variables,]
-
-  d2[statVars] <- lapply(d2[statVars], round, numdecimals)
-
-  if (Relabel) {
-    Data <- ReplaceMissingLabels(Data)
-    labels <- sjlabelled::get_label(Data, def.value = colnames(Data)) %>%
-      as.data.frame() %>%
-      tibble::rownames_to_column()
-    colnames(labels) <- c("Variable", "label")
-    d2$label <- labels$label
-    d2 <- d2 %>% select(label, all_of(statVars))
-  }
-
-  SummaryTable <- d2 %>%
-    rownames_to_column('Variable') %>%
-    mutate(
-      Skewness = cell_spec(round(Skewness, 2), "html", background = if_else(abs(Skewness) > 10, "yellow", "", missing = "grey")),
-      Kurtosis = cell_spec(round(Kurtosis, 2), "html", background = if_else(abs(Kurtosis) > 10, "yellow", "", missing = "grey")),
-      IQR = cell_spec(round(IQR, 2), "html", background = if_else(abs((IQR / Std.Dev / 1.34) - 1) > 0.5, "yellow", "", missing = "grey")),
-      Pct.Valid = cell_spec(round(Pct.Valid, 2), "html", background = if_else(Pct.Valid < 70, "red", "", missing = "grey"))
-    ) %>%
-    kable(format = "html", escape = FALSE, digits = 2, row.names = TRUE,
-          caption = "Descriptive Summary Table. IQR, Skewness, and Kurtosis are highlighted in yellow if they are indicative of a non-normal distribution. Pct.Valid is highlighted in red if over 30% of data is missing") %>%
-    kable_styling(bootstrap_options = c("striped", "hover", "condensed", "responsive")) %>%
-    scroll_box(width = "100%", height = ScrollBoxHeight)
-
-  })
-
-  return(SummaryTable)
-}
