2d9c4153211257561a357a2bfd6d8b5064692a57
RDastgh1 <rdastgh1@jhmi.edu>
2024-08-22 13:54:16 -0400

    specify dplyr use

--- R/CreateSummaryTable.R
+++ R/CreateSummaryTable.R
@@ -13,51 +13,72 @@
 #' @importFrom sjlabelled get_label set_label
 #' @importFrom summarytools descr
 #' @export
+
+
 CreateSummaryTable <- function(Data, Variables = NULL, numdecimals = 2, Relabel = TRUE, Ordinal = FALSE, ScrollBoxHeight = "700px") {
 
-    if (is.null(Variables)) {
-      Variables = colnames(Data)
-    }
-    suppressWarnings({
-      Data <- Data %>% select(all_of(Variables))
-      l <- get_label(Data)
-      if(Ordinal){
-        Data <- ConvertOrdinalToNumeric(Data)
-        Data <- lapply(Data, as.numeric) %>% as.data.frame()
-        if(Relabel){
-          sjlabelled::set_label(Data) <- l
-        }
-      }
-
-      d <- summarytools::descr(Data)
-      statVars <- c("Mean", "Std.Dev", "Median", "IQR", "Min",
-                    "Max", "Skewness", "Kurtosis", "N.Valid", "Pct.Valid")
-      d2 <- as.data.frame(t(as.data.frame(d)))
-      d2 <- d2 %>% select(all_of(statVars))
-      d2 <- d2[Variables, ]
-      d2[statVars] <- lapply(d2[statVars], round, numdecimals)
-      if (Relabel) {
-        Data <- ReplaceMissingLabels(Data)
-        labels <- sjlabelled::get_label(Data, def.value = colnames(Data)) %>%
-          as.data.frame() %>% tibble::rownames_to_column()
-        colnames(labels) <- c("Variable", "label")
-        d2$label <- labels$label
-        d2 <- d2 %>% select(label, all_of(statVars))
-      }
-      SummaryTable <- d2 %>% tibble::rownames_to_column("Variable") %>%
-        mutate(Skewness = kableExtra::cell_spec(round(Skewness, numdecimals), "html",
-                                    background = if_else(abs(Skewness) > 10, "yellow",
-                                                         "", missing = "grey")), Kurtosis = kableExtra::cell_spec(round(Kurtosis,
-                                                                                                            numdecimals), "html", background = if_else(abs(Kurtosis) >
-                                                                                                                                               10, "yellow", "", missing = "grey")), IQR = kableExtra::cell_spec(round(IQR,
-                                                                                                                                                                                                           numdecimals), "html", background = if_else(abs((IQR/Std.Dev/1.34) -
-                                                                                                                                                                                                                                                  1) > 0.5, "yellow", "", missing = "grey")), Pct.Valid = kableExtra::cell_spec(round(Pct.Valid,
-                                                                                                                                                                                                                                                                                                                          numdecimals), "html", background = if_else(Pct.Valid <
-                                                                                                                                                                                                                                                                                                                                                             70, "red", "", missing = "grey"))) %>% kableExtra::kable(format = "html",
-                                                                                                                                                                                                                                                                                                                                                                                                          escape = FALSE, digits = numdecimals, row.names = TRUE, caption = "Descriptive Summary Table. IQR, Skewness, and Kurtosis are highlighted in yellow if they are indicative of a non-normal distribution. Pct.Valid is highlighted in red if over 30% of data is missing") %>%
-        kableExtra::kable_styling(bootstrap_options = c("striped", "hover",
-                                            "condensed", "responsive")) %>% kableExtra::scroll_box(width = "100%",
-                                                                                       height = ScrollBoxHeight)
-    })
-    return(SummaryTable)
+  if (is.null(Variables)) {
+    Variables <- colnames(Data)
   }
+
+  suppressWarnings({
+    Data <- dplyr::select(Data, dplyr::all_of(Variables))
+    l <- sjlabelled::get_label(Data)
+
+    if(Ordinal){
+      Data <- ConvertOrdinalToNumeric(Data)
+      Data <- lapply(Data, as.numeric) %>% as.data.frame()
+      if(Relabel){
+        sjlabelled::set_label(Data) <- l
+      }
+    }
+
+    d <- summarytools::descr(Data)
+    statVars <- c("Mean", "Std.Dev", "Median", "IQR", "Min", "Max", "Skewness", "Kurtosis", "N.Valid", "Pct.Valid")
+    d2 <- as.data.frame(t(as.data.frame(d)))
+    d2 <- dplyr::select(d2, dplyr::all_of(statVars))
+    d2 <- d2[Variables, ]
+    d2[statVars] <- lapply(d2[statVars], round, numdecimals)
+
+    if (Relabel) {
+      Data <- ReplaceMissingLabels(Data)
+      labels <- sjlabelled::get_label(Data, def.value = colnames(Data)) %>%
+        as.data.frame() %>% tibble::rownames_to_column()
+      colnames(labels) <- c("Variable", "label")
+      d2$label <- labels$label
+      d2 <- dplyr::select(d2, label, dplyr::all_of(statVars))
+    }
+
+    SummaryTable <- d2 %>%
+      tibble::rownames_to_column("Variable") %>%
+      dplyr::mutate(
+        Skewness = kableExtra::cell_spec(
+          round(Skewness, numdecimals), "html",
+          background = dplyr::if_else(abs(Skewness) > 10, "yellow", "", missing = "grey")
+        ),
+        Kurtosis = kableExtra::cell_spec(
+          round(Kurtosis, numdecimals), "html",
+          background = dplyr::if_else(abs(Kurtosis) > 10, "yellow", "", missing = "grey")
+        ),
+        IQR = kableExtra::cell_spec(
+          round(IQR, numdecimals), "html",
+          background = dplyr::if_else(abs((IQR/Std.Dev/1.34) - 1) > 0.5, "yellow", "", missing = "grey")
+        ),
+        Pct.Valid = kableExtra::cell_spec(
+          round(Pct.Valid, numdecimals), "html",
+          background = dplyr::if_else(Pct.Valid < 70, "red", "", missing = "grey")
+        )
+      ) %>%
+      kableExtra::kable(
+        format = "html", escape = FALSE, digits = numdecimals,
+        row.names = TRUE,
+        caption = "Descriptive Summary Table. IQR, Skewness, and Kurtosis are highlighted in yellow if they are indicative of a non-normal distribution. Pct.Valid is highlighted in red if over 30% of data is missing"
+      ) %>%
+      kableExtra::kable_styling(
+        bootstrap_options = c("striped", "hover", "condensed", "responsive")
+      ) %>%
+      kableExtra::scroll_box(width = "100%", height = ScrollBoxHeight)
+  })
+
+  return(SummaryTable)
+}
