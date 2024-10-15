bf7640b871633c3276b5e35f31bac11bedb529bb
RDastgh1 <rdastgh1@jhmi.edu>
2024-10-15 15:55:43 -0400

    specify dplyr for filter

--- R/PlotAnovaRelationshipsMatrix.R
+++ R/PlotAnovaRelationshipsMatrix.R
@@ -122,7 +122,7 @@ PlotAnovaRelationshipsMatrix <- function(Data, CatVars, ContVars, Covariates = N
   library(ggplot2)
   library(paletteer)
 
-  stat.test <- stat.test %>% ungroup() %>% ungroup () %>% as.data.frame() %>% filter(Effect == "CategoricalValue")
+  stat.test <- stat.test %>% ungroup() %>% ungroup () %>% as.data.frame() %>% dplyr::filter(Effect == "CategoricalValue")
 
   # Modify your plot with additional adjustments for better color distinction
   p <- ggplot(stat.test, aes(y = YLabel, x = XLabel, shape = `p<.05`, text = PlotText)) +
