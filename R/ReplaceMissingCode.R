d7c2363aded53939ea5aefdadc3424ebe85b9cb2
RDastgh1 <rdastgh1@jhmi.edu>
2025-03-23 17:01:46 -0400

    update replacemissing so it can have multiple values

--- R/ReplaceMissingCode.R
+++ R/ReplaceMissingCode.R
@@ -9,11 +9,20 @@
 
 #' @export
 ReplaceMissingCode <- function(DataFrame, VariableCodebook) {
+  # Filter out rows where MissingCode is NA
   CodeSubset <- VariableCodebook %>% dplyr::filter(!is.na(MissingCode))
+
+  # Loop through each row of the codebook
   for (i in 1:nrow(CodeSubset)) {
-    Var <- CodeSubset$Variable[i]
-    MC <- CodeSubset$MissingCode[i]
-    DataFrame[[Var]][DataFrame[[Var]] == MC] <- NA
+    Var <- CodeSubset$Variable[i]  # The variable name
+    MissingCodes <- strsplit(CodeSubset$MissingCode[i], ",\\s*")[[1]]  # Split by commas and handle spaces
+
+    # Loop through each code in MissingCodes and replace with NA
+    for (MC in MissingCodes) {
+      MC <- as.numeric(MC)  # Convert to numeric in case the missing code is represented as a number
+      DataFrame[[Var]][DataFrame[[Var]] == MC] <- NA
+    }
   }
+
   return(DataFrame)
 }
